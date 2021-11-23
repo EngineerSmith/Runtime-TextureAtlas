92d945b8b2d9273d20e1287e8bc1c075f3be3043
EngineerSmith <pasmith.bus@gmail.com>
2021-11-23 20:49:01 +0000

    Switched 4 spaced tabs for 2 spaced tabs

--- baseAtlas.lua
+++ baseAtlas.lua
@@ -1,123 +1,124 @@
 -- Copyright (c) 2021 EngineerSmith
 -- Under the MIT license, see license suppiled with this file
 
+local lg = love.graphics
+
 local baseAtlas = {
-    _canvasSettings = {
-        dpiscale = 1,
-    },
-    _maxCanvasSize =  love.graphics.getSystemLimits().texturesize -1
+  _canvasSettings = {
+    dpiscale = 1,
+  },
+  _maxCanvasSize =  lg.getSystemLimits().texturesize -1
+  _arrayTextureSupport = lg.getTextureTypes()["array"]
 }
 baseAtlas.__index = baseAtlas
 
-local lg = love.graphics
-
 baseAtlas.new = function(padding)
-    return setmetatable({
-        padding = padding or 1,
-        image,
-        images = {},
-        imagesSize = 0,
-        ids = {},
-        quads = {},
-        filterMin = "linear", 
-        filterMag = "linear",
-        _dirty = false, -- Marked dirty if image is added or removed,
-        _hardBake = false, -- Marked true if hardBake has been called, cannot use add, remove or bake once true
-    }, baseAtlas)
+  return setmetatable({
+    padding = padding or 1,
+    image,
+    images = {},
+    imagesSize = 0,
+    ids = {},
+    quads = {},
+    filterMin = "linear", 
+    filterMag = "linear",
+    _dirty = false, -- Marked dirty if image is added or removed,
+    _hardBake = false, -- Marked true if hardBake has been called, cannot use add, remove or bake once true
+  }, baseAtlas)
 end
 
 -- TA:add(img, "foo")
 -- TA:add(img, 68513, true)
 baseAtlas.add = function(self, image, id, bake, ...)
-    if self._hardBake then
-        error("Cannot add images to a texture atlas that has been hard baked")
-    end
-    self.imagesSize = self.imagesSize + 1
-    local index = self.imagesSize
-    assert(type(id) ~= "nil", "Must give an id")
-    self:remove(id)
-    self.images[index] = {
-        image = image,
-        id = id,
-        index = index,
-    }
-    self.ids[id] = index
-    
-    self._dirty = true
-    if bake then
-        self:bake(...)
-    end
-    
-    return self
+  if self._hardBake then
+    error("Cannot add images to a texture atlas that has been hard baked")
+  end
+  self.imagesSize = self.imagesSize + 1
+  local index = self.imagesSize
+  assert(type(id) ~= "nil", "Must give an id")
+  self:remove(id)
+  self.images[index] = {
+    image = image,
+    id = id,
+    index = index,
+  }
+  self.ids[id] = index
+  
+  self._dirty = true
+  if bake then
+    self:bake(...)
+  end
+  
+  return self
 end
 
 -- TA:remove("foo", true)
 -- TA:remove(68513)
 baseAtlas.remove = function(self, id, bake, ...)
-    if self._hardBake then
-        error("Cannot remove images from a texture atlas that has been hard baked")
+  if self._hardBake then
+    error("Cannot remove images from a texture atlas that has been hard baked")
+  end
+  local index = self.ids[id]
+  if index then
+    self.images[index] = nil
+    self.quads[id] = nil
+    self.ids[id] = nil
+    self._dirty = true
+    if bake == true then
+      self:bake(...)
     end
-    local index = self.ids[id]
-    if index then
-        self.images[index] = nil
-        self.quads[id] = nil
-        self.ids[id] = nil
-        self._dirty = true
-        if bake == true then
-            self:bake(...)
-        end
-    end
-    
-    return self
+  end
+  
+  return self
 end
 
 baseAtlas.bake = function(self)
-    error("Warning! Created atlas hasn't overriden bake function!")
-    return self
+  error("Warning! Created atlas hasn't overriden bake function!")
+  return self
 end
 
 baseAtlas.hardBake = function(self, ...)
-    self:bake(...)
-    self.images = nil
-    self.ids = nil
-    self._hardBake = true
-    return self
+  self:bake(...)
+  self.images = nil
+  self.ids = nil
+  self._hardBake = true
+  return self
 end
 
 -- returns position on texture atlas, x,y, w,h
 baseAtlas.getViewport = function(self, id)
-    local quad = self.quads[id]
-    if quad then
-        return quad:getViewport()
-    end
-    error("Warning! Quad hasn't been baked for id: " .. tostring(id))
+  local quad = self.quads[id]
+  if quad then
+    return quad:getViewport()
+  end
+  error("Warning! Quad hasn't been baked for id: " .. tostring(id))
 end
 
 baseAtlas.setFilter = function(self, min, mag)
-    self.filterMin = min or "linear"
-    self.filterMag = mag or self.filterMin
-    if self.image then
-        self.image:setFilter(self.filterMin, self.filterMag)
-    end
-    return self
+  self.filterMin = min or "linear"
+  self.filterMag = mag or self.filterMin
+  if self.image then
+    self.image:setFilter(self.filterMin, self.filterMag)
+  end
+  return self
 end
 
 baseAtlas.draw = function(self, id, ...)
-    lg.draw(self.image, self.quads[id], ...)
+  lg.draw(self.image, self.quads[id], ...)
 end
 
 -- Following functions are if you don't want to pass textureAtlas or ids around
 
 baseAtlas.getDrawFunc = function(self)
-    return function(...) -- uses baseAtlas.draw's args (id, ...)
-        self:draw(...)
-    end
+  return function(...) -- uses baseAtlas.draw's args (id, ...)
+    self:draw(...)
+  end
 end
 
 baseAtlas.getDrawFuncForID = function(self, id)
-    return function(...)
-        lg.draw(self.image, self.quads[id], ...)
-    end
+  return function(...)
+    lg.draw(self.image, self.quads[id], ...)
+  end
 end
 
 return baseAtlas
--- fixedSize.lua
+++ fixedSize.lua
@@ -10,66 +10,67 @@ local lg = love.graphics
 local ceil, floor, sqrt = math.ceil, math.sqrt, math.floor
 
 fixedSizeTA.new = function(width, height, padding)
-    local self = setmetatable(baseAtlas.new(padding), fixedSizeTA)
-    self.width = width or error("Width required")
-    self.height = height or width
-    return self
+  local self = setmetatable(baseAtlas.new(padding), fixedSizeTA)
+  self.width = width or error("Width required")
+  self.height = height or width
+  return self
 end
 
 fixedSizeTA.add = function(self, image, id, bake)
-    local width, height = image:getDimensions()
-    if width ~= self.width or height ~= self.height then
-        error("Given image cannot fit into a fixed sized texture atlas\n Gave: W:".. tostring(width) .. " H:" ..tostring(height) .. ", Expected: W:"..self.width.." H:"..self.height)
-    end
-    return baseAtlas.add(self, image, id, bake)
+  local width, height = image:getDimensions()
+  if width ~= self.width or height ~= self.height then
+    error("Given image cannot fit into a fixed sized texture atlas\n Gave: W:".. tostring(width) .. " H:" ..tostring(height) .. ", Expected: W:"..self.width.." H:"..self.height)
+  end
+  return baseAtlas.add(self, image, id, bake)
 end
 
 fixedSizeTA.bake = function(self)
-    if self._dirty and not self._hardBake then
-        local columns = ceil(sqrt(self.imagesSize))
-        local width, height = self.width, self.height
-        local widthPadded, heightPadded = width + self.padding, height + self.padding
-        
-        local widthCanvas = columns * widthPadded
-        if widthCanvas > self._maxCanvasSize then
-            columns = floor(self._maxCanvasSize / width)
-            widthCanvas = columns * widthPadded
-        end
-            
-        local rows = ceil(self.imagesSize / columns)
-        local heightCanvas = rows * heightPadded
-        if heightPadded > self._maxCanvasSize then
-            rows = floor(self._maxCanvasSize / height)
-            heightCanvas = rows * heightPadded
-        end
-        
-        if columns * rows < self.imagesSize then
-            error("Cannot support "..tostring(self.imagesSize).." images, due to system limits of canvas size. Max allowed on this system: "..tostring(columns * rows))
-        end
-        
-        local canvas = lg.newCanvas(widthCanvas, heightCanvas, self._canvasSettings)
-        local maxIndex = self.imagesSize
-        lg.push("all")
-        lg.setCanvas(canvas)
-        for x=0, rows-1, 1 do
-            for y=0, columns-1, 1 do
-                local index = (x+y*columns)+1
-                if index > maxIndex then
-                    break
-                end
-                local x, y = x*widthPadded, y*heightPadded
-                local image = self.images[index]
-                lg.draw(image.image, x, y)
-                self.quads[image.id] = lg.newQuad(x, y, width, height, widthCanvas, heightCanvas)
-            end
-        end
-        lg.pop()
-        self.image = lg.newImage(canvas:newImageData())
-        self.image:setFilter(self.filterMin, self.filterMag)
-        self._dirty = false
+  if self._dirty and not self._hardBake then
+    local columns = ceil(sqrt(self.imagesSize))
+    local width, height = self.width, self.height
+    local widthPadded, heightPadded = width + self.padding, height + self.padding
+    
+    local widthCanvas = columns * widthPadded
+    if widthCanvas > self._maxCanvasSize then
+      columns = floor(self._maxCanvasSize / width)
+      widthCanvas = columns * widthPadded
     end
     
-    return self
+    local rows = ceil(self.imagesSize / columns)
+    local heightCanvas = rows * heightPadded
+    if heightPadded > self._maxCanvasSize then
+      rows = floor(self._maxCanvasSize / height)
+      heightCanvas = rows * heightPadded
+    end
+    
+    if columns * rows < self.imagesSize then
+      
+      error("Cannot support "..tostring(self.imagesSize).." images, due to system limits of canvas size. Max allowed on this system: "..tostring(columns * rows))
+    end
+    
+    local canvas = lg.newCanvas(widthCanvas, heightCanvas, self._canvasSettings)
+    local maxIndex = self.imagesSize
+    lg.push("all")
+    lg.setCanvas(canvas)
+    for x=0, rows-1, 1 do
+      for y=0, columns-1, 1 do
+        local index = (x+y*columns)+1
+        if index > maxIndex then
+          break
+        end
+        local x, y = x*widthPadded, y*heightPadded
+        local image = self.images[index]
+        lg.draw(image.image, x, y)
+        self.quads[image.id] = lg.newQuad(x, y, width, height, widthCanvas, heightCanvas)
+      end
+    end
+    lg.pop()
+    self.image = lg.newImage(canvas:newImageData())
+    self.image:setFilter(self.filterMin, self.filterMag)
+    self._dirty = false
+  end
+  
+  return self
 end
 
 return fixedSizeTA
