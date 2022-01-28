49b7d3f3f0f83e8b8094c649dd22aad5ec60bf8d
EngineerSmith <pasmith.bus@gmail.com>
2022-01-28 12:01:32 +0000

    Covered filling and spliting unoccupied cells

--- packing.lua
+++ packing.lua
@@ -4,6 +4,8 @@
 -- Custom algorithm to solve the rectangle packing problem
 -- As every other algorithm only solved the bin packing problem
 
+local insert, remove = table.insert, table.remove
+
 local cell = {}
 cell.__index = {}
 
@@ -21,7 +23,7 @@ grid.new = function(limitWidth, limitHeight)
   return setmetatable({
       limitWidth  = limitWidth,
       limitHeight = limitHeight,
-      currentWidth = 0,
+      currentWidth  = 0,
       currentHeight = 0,
       cells = {},
       unoccupiedCells = {},
@@ -29,15 +31,30 @@ grid.new = function(limitWidth, limitHeight)
 end
 
 grid.insert = function(self, width, height, image)
-  for index, cell in ipairs(self.unoccupiedCells) do
-    if cell.w == width and cell.h >= height then
-      -- split and add
+  for index, unoccupiedCell in ipairs(self.unoccupiedCells) do
+    if unoccupiedCell.w == width and unoccupiedCell.h == height then
+      unoccupiedCell.image = image
+      remove(self.unoccupiedCells, index)
+      insert(self.cells, unoccupiedCell)
       return
-    elseif cell.h == height and cell.w >= width then
-      -- split and add
+    elseif cell.w == width and cell.h > height then
+      insert(self.unoccupiedCells, cell.new(unoccupiedCell.x, unoccupiedCell.y+height, width, height-unoccupiedCell.h))
+      remove(self.unoccupiedCells, index)
+      unoccupiedCell.h, unoccupiedCell.image = height, image
+      insert(self.cells, unoccupiedCell) -- top
       return
-    elseif cell.w < width and cell.h < height then
+    elseif cell.h == height and cell.w > width then
+      insert(self.unoccupiedCells, cell.new(unoccupiedCell.x+width, unoccupiedCell.y, width-unoccupiedCell.w, height))
+      remove(self.unoccupiedCells, index)
+      unoccupiedCell.w, unoccupiedCell.image = width, image
+      insert(self.cells, unoccupiedCell)
+      return
+    elseif cell.w > width and cell.h > height then
       -- split and add
+      insert(self.unoccupiedCells, cell.new(unoccupiedCell.x+width, unoccupiedCell.y+height, width-unoccupiedCell.w, height-unoccupiedCell.h))
+      remove(self.unoccupiedCells, index)
+      unoccupiedCell.w, unoccupiedCell.h, unoccupiedCell.image = width, height, image
+      insert(self.cells, unoccupiedCell)
       return
     end
   end
